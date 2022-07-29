fc022ad950967bf79e2a05e3097116d18e4a01df
cbinckly <cbinckly@gmail.com>
2022-07-29 09:51:28 -0400

    rename to aio-api-sm to bypass pypi naming conflict.

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="aiorm", # Replace with your own username
+    name="aio-api-sm", # Replace with your own username
     version="1.0",
     author="cbinckly",
     author_email="cbinckly@gmail.com",
-    packages=['aiorm'],
+    packages=['aio-api-sm'],
     install_requires=[
         'python-dateutil',
         'aiohttp',
     ],
-    description="Asynchronous HTTP Request Manager.",
+    description="Asynchronous HTTP Session Manager with retry, rate limiting, and more.",
     long_description=long_description,
-    long_description_content_type="text/md",
+    long_description_content_type="text/markdown",
     url="https://aiorm.rtfd.io",
     classifiers=[
         "Programming Language :: Python :: 3",
